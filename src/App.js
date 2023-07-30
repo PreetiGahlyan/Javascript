1965e57dd2b21a5c6d1fec99dd88cc69968350ef
Preeti Tandon <preeti.tandon@angloamerican.com>
2023-07-30 14:05:09 +0200

    refactored to render squares dynamically using 2 for loops

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function Game(){
+export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
   const xIsNext = currentMove % 2 === 0;
@@ -19,42 +19,46 @@ export default function Game(){
   const moves = history.map((squares, move) => {
     let description;
     if (move > 0) {
-      description = 'Go to move #' + move;
+      description = "Go to move #" + move;
     } else {
-      description = 'Go to game start';
+      description = "Go to game start";
     }
     return (
       <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
+        {move == currentMove ? (
+          <div>You are at move # {move}</div>
+        ) : (
+          <button onClick={() => jumpTo(move)}>{description}</button>
+        )}
       </li>
     );
   });
 
-
-  return(
+  return (
     <div className="game">
       <div className="game-board">
-      <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
         <ol>{moves}</ol>
       </div>
     </div>
-    
   );
 }
 
- function Board({xIsNext, squares, onPlay}) {
+function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
     if (squares[i] || calculateWinner(squares)) {
       return;
     }
     const nextSquares = squares.slice();
+
     if (xIsNext) {
-      nextSquares[i] = "X";
-    } else {
-      nextSquares[i] = "O";
+    nextSquares[i] = "X";
     }
+    else
+    nextSquares[i] ="O";
+  
     onPlay(nextSquares);
   }
 
@@ -66,32 +70,45 @@ export default function Game(){
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
 
+  function renderSquare(i) {
+    return (
+      <Square 
+        key={i}
+        value={squares[i]} 
+        onSquareClick={() => handleClick(i)}
+      />
+    );
+  };
+
+ function renderBoard(size) {
+    const board = [];
+    for (let i = 0; i < size; i++) {
+      let row = [];
+      for (let j = 0; j < size; j++) {
+        row.push(renderSquare(i * size + j));
+      }
+      board.push(
+        <div key={i} className="board-row">
+          {row}
+        </div>
+      )
+    }
+    console.log({board})
+    return board;
+  }
+
     return (
       <>
-   <div className="status">{status}</div>
-        <div className="board-row"> 
-          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-          <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-          <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-        </div>
-        <div className="board-row">
-          <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-          <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-          <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-        </div>
-        <div className="board-row">
-          <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-          <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-          <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-         </div>
+      <div className="status">{status}</div>
+      <div>
+        {renderBoard(3)}
+      </div>
       </>
     );
-
- 
-
 }
 
-function Square({ value, onSquareClick }) {
+
+function Square({  value, onSquareClick }) {
   return (
     <button className="square" onClick={onSquareClick}>
       {value}
