8f452d97f786ecbe9d5b7489db65e2a5c3d4c392
Preeti Tandon <preeti.tandon@angloamerican.com>
2023-11-15 14:53:14 +0100

    add function to find longest common prefix in a array of strings

--- index.js
+++ index.js
@@ -29,5 +29,31 @@ function intToRoman(num) {
   return output
 }
 
-const result = intToRoman(2500)
-console.log(result)
+console.log("Integar 2500 converted to roman numeral is: ", intToRoman(2500))
+
+function longestCommonPrefix(strings) {
+  if (strings.length === 0) {
+    return ""
+  }
+
+  // Sort the array to easily find the common prefix
+  strings.sort()
+
+  // Take the first and last strings in the sorted array
+  const firstStr = strings[0]
+  const lastStr = strings[strings.length - 1]
+
+  // Find the common prefix between the first and last strings
+  let commonPrefix = ""
+  for (let i = 0; i < firstStr.length; i++) {
+    if (firstStr[i] === lastStr[i]) {
+      commonPrefix += firstStr[i]
+    } else {
+      break
+    }
+  }
+
+  return commonPrefix
+}
+
+console.log(longestCommonPrefix(["flower", "flow", "flight"]))
